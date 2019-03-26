7d1d51189f19b9cd8e7e7ed7dadec7def1de3a8c
Hayk Mikayelyan <mikahayk@gmail.com>
2019-03-26 19:45:14 -0400

    main script

--- main-script.py
+++ main-script.py
@@ -1,21 +1,46 @@
-from picamera import PiCamera
-from time import sleep
-
-camera = PiCamera()
-
-##camera.start_preview()
-##sleep(10)
-##camera.stop_preview()
+import io
+import picamera
+import cv2
+import numpy
+import datetime
+import time
 
 
-##camera.start_preview()
-##sleep(5)
-##camera.capture('/home/pi/Desktop/image.jpg')
-##camera.stop_preview()
+while True:
+    #Create a memory stream so photos doesn't need to be saved in a file
+    stream = io.BytesIO()
+    #Get the picture (low resolution, so it should be quite fast)
+    #Here you can also specify other parameters (e.g.:rotate the image)
+    with picamera.PiCamera() as camera:
+        camera.resolution = (320, 240)
+        camera.capture(stream, format='jpeg')
 
+    #Convert the picture into a numpy array
+    buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
 
-camera.start_preview()
-for i in range(5):
-    sleep(1)
-    camera.capture('/home/pi/Desktop/image%s.jpg' % i)
-camera.stop_preview()
+    #Now creates an OpenCV image
+    image = cv2.imdecode(buff, 1)
+
+    #Load a cascade file for detecting faces
+    face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+
+    #Convert to grayscale
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    #Look for faces in the image using the loaded cascade file
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    if len(faces) > 0:
+        print("Found "+str(len(faces))+" face")
+        break
+    else:
+        print("No face found")
+    
+cv2.putText(image, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, image.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)    
+    
+#Draw a rectangle around every found face
+for (x,y,w,h) in faces:
+    cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
+    
+    
+#Save the result image
+cv2.imwrite('result.jpg',image)
